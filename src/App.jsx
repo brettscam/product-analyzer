d570c733d87eb8292863060e6461ade6b64a0089
brettscam <brettcameronsmith@gmail.com>
2024-11-10 19:17:13 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import PlasticProductAnalysis from './components/PlasticProductAnalysis'
+import React from 'react';
+import CombinedAnalyzer from './components/CombinedAnalyzer';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
-      <PlasticProductAnalysis />
+      <CombinedAnalyzer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

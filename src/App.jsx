3866b9fac38f005c15671a07fe3f3a6f31d873ab
brettscam <brettcameronsmith@gmail.com>
2024-11-10 19:36:35 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,2 @@
 import React from 'react';
 import CombinedAnalyzer from './components/CombinedAnalyzer';
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <CombinedAnalyzer />
-    </div>
-  );
-}
-
-export default App;

95d11a65f51482310a1d22615a4df1347af95b53
brettscam <brettcameronsmith@gmail.com>
2024-11-10 19:29:31 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-import React, { useState } from 'react';
-import PlasticProductAnalysis from './PlasticProductAnalysis';
-import ProductAnalyzer from './ProductAnalyzer';
-import ProductTypeSelector from './ProductTypeSelector';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const CombinedAnalyzer = () => {
-  const [analysisType, setAnalysisType] = useState('plastic');
-  
-  const renderAnalyzer = () => {
-    switch(analysisType) {
-      case 'plastic':
-        return <PlasticProductAnalysis />;
-      case 'ingredients':
-      case 'pregnancy':
-        return <ProductAnalyzer mode={analysisType} />;
-      default:
-        return <PlasticProductAnalysis />;
-    }
-  };
-
-  return (
-    <div className="max-w-4xl mx-auto mt-10 p-6 bg-white rounded-lg shadow-xl">
-      <h2 className="text-2xl font-bold mb-6">Product Safety Analyzer</h2>
-      
-      <ProductTypeSelector 
-        onTypeSelect={setAnalysisType}
-        selectedType={analysisType}
-      />
-
-      {renderAnalyzer()}
-    </div>
-  );
-};
-
-export default CombinedAnalyzer;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

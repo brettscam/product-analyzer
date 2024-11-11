4b5c734284c829a5077947c7490fcb905f41bfd4
brettscam <brettcameronsmith@gmail.com>
2024-11-11 13:23:52 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
         main: './index.html',
       },
-      output: {
-        format: 'es',
-      },
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
   },
 });

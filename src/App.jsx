2b6879919732c088cbe843839ac0851ff6b2f35d
brettscam <brettcameronsmith@gmail.com>
2024-11-10 17:03:29 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { ProductAnalyzer } from './components/ProductAnalyzer'
-import PlasticProductAnalysis from './components/PlasticProductAnalysis';
+import { PlasticProductAnalysis } from './components/PlasticProductAnalysis'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
-      <ProductAnalyzer />
       <PlasticProductAnalysis />
     </div>
   )

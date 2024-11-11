923ef661a1ddb7c40b6b2c4446f324a4fa92003e
brettscam <brettcameronsmith@gmail.com>
2024-11-11 13:07:15 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import CombinedAnalyzer from './components/CombinedAnalyzer';
+import ProductTypeSelector from './components/ProductTypeSelector';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductTypeSelector />
+      <CombinedAnalyzer />
+    </div>
+  );
+}
+
+export default App;

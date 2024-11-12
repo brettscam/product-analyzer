3c0708939f8ecea2ab2473a351146025726e508d
brettscam <brettcameronsmith@gmail.com>
2024-11-11 21:41:30 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import ErrorBoundary from './components/ErrorBoundary';
 import CombinedAnalyzer from './components/CombinedAnalyzer';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <CombinedAnalyzer />
+      <ErrorBoundary>
+        <CombinedAnalyzer />
+      </ErrorBoundary>
     </div>
   );
 }
